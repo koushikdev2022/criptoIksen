f8d24020884b23b8854a3613f6c6885a0b0aa4d9
sourav16716 <54978571+sourav16716@users.noreply.github.com>
2025-07-23 18:27:21 +0530

    update design
    update design

--- app/ui/footer.js
+++ app/ui/footer.js
@@ -41,7 +41,7 @@ const Footer = () => {
            <div className='footer_top_container lg:flex gap-20'>
               <div className='lg:w-4/12 mb-6 lg:mb-0 text-center lg:text-left'>
                 <Image src={footerLogo} alt='footerLogo' className='inline-block w-4/12 mb-2' />
-                <p className='text-sm text-white font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                <p className='text-sm text-white font-medium'>CryptoIntuit was born from the frustration of digging through data and charts. We built a platform that does the heavy lifting for you — so you can make faster, smarter decisions with clarity. </p>
               </div>
               <div className='lg:w-8/12 lg:flex'>
                  <div className='lg:w-6/12 text-center lg:text-left mb-6 lg:mb-0'>
